2f422a8b89593b045bfd065a4d9ac7ab5864c049
Ben Fradet <benjamin.fradet@gmail.com>
2018-05-16 10:18:24 +0100

    PR stream creation

--- src/main/scala/clabot/Main.scala
+++ src/main/scala/clabot/Main.scala
@@ -1,11 +1,16 @@
 package clabot
 
+import cats.effect.IO
 import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder
+import com.amazonaws.services.sqs.model._
 import com.typesafe.config.ConfigFactory
-import io.circe.generic.auto._
+import fs2.Stream
 import io.circe.config.syntax._
+import io.circe.generic.auto._
+import io.circe.parser._
 
 import config._
+import model._
 
 object Main {
   private val client = AmazonSQSAsyncClientBuilder.defaultClient()
@@ -24,6 +29,23 @@ object Main {
 
   def main(config: ClaBotConfig): Unit = {
     // get pr event stream
+    val rmr = new ReceiveMessageRequest(config.aws.sqsQueueUrl)
+      .withMaxNumberOfMessages(1)
+      .withWaitTimeSeconds(10)
+    val sqsStream: Stream[IO, Message] = sqs.messageStream(client, rmr)
+
+    val pullRequestStream: Stream[IO, PR] = sqsStream
+      .map(m => decode[PullRequestEvent](m.getBody))
+      .collect { case Right(pr) => pr }
+      .map { event => (
+        event.pull_request.base.flatMap(_.repo),
+        event.number
+      ) }
+      .collect { case (Some(repo), n) =>
+        val Array(owner, repoName) = repo.full_name.split("/")
+        PR(owner, repoName, n)
+      }
+
     // filter pr creator who are in the org
     // check the google sheet
     // post a message saying yes / no
--- src/main/scala/clabot/model.scala
+++ src/main/scala/clabot/model.scala
@@ -1,11 +1,4 @@
-/**
- * PROPRIETARY AND CONFIDENTIAL
- *
- * Unauthorized copying of this file via any medium is strictly prohibited.
- *
- * Copyright (c) 2017-2017 Snowplow Analytics Ltd. All rights reserved.
- */
-package com.snowplowanalytics.srs
+package clabot
 
 import github4s.free.domain.PullRequest
 
@@ -27,12 +20,10 @@ object model {
    * @param owner of the repository
    * @param repo name of the repository
    * @param number of the pull pull request
-   * @param sha of the commit at the PR's head
    */
   case class PR(
     owner: String,
     repo: String,
-    number: Int,
-    sha: String
+    number: Int
   )
 }
