e0036560bdfce35a5732d6a5742a490786fde470
Andrzej Sołtysik <andrzej.soltysik17@gmail.com>
2018-08-17 10:06:05 +0200

    Replace SQS integration with a webhook (closes #8)

--- build.sbt
+++ build.sbt
@@ -8,6 +8,7 @@ lazy val compilerOptions = Seq(
   "-language:higherKinds",
   "-unchecked",
   "-Yno-adapted-args",
+  "-Ypartial-unification",
   "-Ywarn-dead-code",
   "-Ywarn-numeric-widen",
   "-Ywarn-unused-import",
@@ -26,36 +27,34 @@ lazy val baseSettings = Seq(
   version := "0.1.0-SNAPSHOT"
 )
 
-lazy val fs2Version = "0.10.4"
-lazy val circeVersion = "0.9.3"
-lazy val github4sVersion = "0.18.4"
+lazy val pureConfigVersion = "0.9.1"
+lazy val http4sVersion = "0.19.0-M1"
+lazy val github4sVersion = "0.18.6"
 lazy val gsheeets4sVersion = "0.1.0-SNAPSHOT"
-lazy val circeConfigVersion = "0.4.1"
 lazy val scalajHttpVersion = "2.4.0"
-lazy val json4sVersion = "3.5.3"
-lazy val awsSdkVersion = "1.11.301"
 lazy val scalatestVersion = "3.0.5"
 lazy val mockitoVersion = "2.17.0"
 
 lazy val claBot = project.in(file("."))
-  .settings(name := "cla-bot")
+  .settings(
+    name := "cla-bot",
+    addCompilerPlugin("org.scalamacros" % "paradise" % "2.1.0" cross CrossVersion.full)
+  )
   .settings(baseSettings)
   .settings(
     libraryDependencies ++= Seq(
-      "co.fs2" %% "fs2-core" % fs2Version,
-      "com.47deg" %% "github4s" % github4sVersion,
+      "com.github.pureconfig" %% "pureconfig" % pureConfigVersion,
+
+      "org.http4s" %% "http4s-dsl"          % http4sVersion,
+      "org.http4s" %% "http4s-blaze-server" % http4sVersion,
+      "org.http4s" %% "http4s-circe"        % http4sVersion,
+
+      "com.47deg"  %% "github4s-cats-effect" % github4sVersion,
+      "org.scalaj" %% "scalaj-http"          % scalajHttpVersion,
+
       "com.github.benfradet" %% "gsheets4s" % gsheeets4sVersion,
-      "io.circe" %% "circe-config" % circeConfigVersion,
-      "org.scalaj" %% "scalaj-http" % scalajHttpVersion,
-      "org.json4s" %% "json4s-native" % json4sVersion,
-      "org.json4s" %% "json4s-jackson" % json4sVersion,
-      "com.amazonaws" % "aws-java-sdk-sqs" % awsSdkVersion
-    ) ++ Seq(
-      "io.circe" %% "circe-core",
-      "io.circe" %% "circe-generic",
-      "io.circe" %% "circe-parser"
-    ).map(_ % circeVersion) ++ Seq(
-      "org.scalatest" %% "scalatest" % scalatestVersion,
-      "org.mockito" % "mockito-core" % mockitoVersion
-    ).map(_ % "test")
+
+      "org.scalatest" %% "scalatest"    % scalatestVersion % "test",
+      "org.mockito"   %  "mockito-core" % mockitoVersion   % "test"
+    )
   )
--- src/main/scala/clabot/model.scala
+++ src/main/scala/clabot/model.scala
@@ -1,45 +1,44 @@
 package clabot
 
+import io.circe.generic.JsonCodec
+
 object model {
+
   /**
-   * Event emitted by the github hooks for pull request.
+   * Event emitted by the github webhook for pull request.
    * @param action which triggered the event, can be opened, reopened, synchronized, closed
    * @param number of the pull request
-   * @param pull_request data about the PR in a Github4s' PullRequest
+   * @param repository data about the repository
+   * @param sender data about the creator of the PR
+   * @param organization data about the organization the repository is in, available only when there is an organization
    */
-  case class PullRequestEvent(
+  @JsonCodec final case class PullRequestEvent(
     action: String,
     number: Int,
     repository: Repository,
-    sender: Sender
+    sender: User,
+    organization: Option[Organization]
   )
 
-  case class IssueCommentEvent(
-    action: String,
-    issue: Issue,
-    comment: Comment,
-    repository: Repository,
-    sender: Sender
-  )
-
-  case class Issue(url: String, number: Int)
-  case class Comment(body: String)
-  case class Repository(full_name: String)
-  case class Sender(login: String)
 
   /**
-   * Internal representation of a PR with only the necessary components.
-   * @param owner of the repository
-   * @param repo name of the repository
-   * @param creator name of the pull request creator
-   * @param number of the pull pull request
-   * @param body contents of the pull request comment (if applicable)
-   */
-  case class PR(
-    owner: String,
-    repo: String,
-    creator: String,
-    number: Int,
-    body: Option[String]
+    * Event emitted by the github webhook for issues (the PR is also treated as an issue). Used here for listening to
+    * PR comments.
+    * @param action what triggered the event, we are only interested in `created`
+    * @param issue issue object containing the issue number
+    * @param repository data about the repository
+    * @param sender data about the commenter
+    */
+  @JsonCodec final case class IssueCommentEvent(
+    action: String,
+    issue: Issue,
+    repository: Repository,
+    sender: User
   )
+
+
+  @JsonCodec final case class Issue(number: Int)
+  @JsonCodec final case class Organization(login: String)
+  @JsonCodec final case class Repository(name: String, owner: User)
+  @JsonCodec final case class User(login: String) // shared between `sender` and `owner` fields
 }
